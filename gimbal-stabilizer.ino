30bbd71d67fc71b5f456a740208d1d2e8b2c86a7
Rayhan Jufahri <rayhanjufahri11@gmail.com>
2021-01-02 16:43:32 +0700

    add explanation & small refactor

--- gimbal-stabilizer.ino
+++ gimbal-stabilizer.ino
@@ -1,3 +1,6 @@
+/* Code for Gimbal Self-stabilizer
+ * 2-axis Servo (pitch & roll)
+ * using MPU6050 and PID control */
   #include <Wire.h>              //Library buat komunikasi I2C
 #include <Servo.h>
 
@@ -5,19 +8,19 @@
 
 #define MPU6050_ADDRESS             0x68
 #define MPU6050_RA_ACCEL_XOUT_H     0x3B
-#define MPU6050_RA_ACCEL_XOUT_L     0x3C
-#define MPU6050_RA_ACCEL_YOUT_H     0x3D
-#define MPU6050_RA_ACCEL_YOUT_L     0x3E
-#define MPU6050_RA_ACCEL_ZOUT_H     0x3F
-#define MPU6050_RA_ACCEL_ZOUT_L     0x40
-#define MPU6050_RA_TEMP_OUT_H       0x41
-#define MPU6050_RA_TEMP_OUT_L       0x42
+//#define MPU6050_RA_ACCEL_XOUT_L     0x3C
+//#define MPU6050_RA_ACCEL_YOUT_H     0x3D
+//#define MPU6050_RA_ACCEL_YOUT_L     0x3E
+//#define MPU6050_RA_ACCEL_ZOUT_H     0x3F
+//#define MPU6050_RA_ACCEL_ZOUT_L     0x40
+//#define MPU6050_RA_TEMP_OUT_H       0x41
+//#define MPU6050_RA_TEMP_OUT_L       0x42
 #define MPU6050_RA_GYRO_XOUT_H      0x43
-#define MPU6050_RA_GYRO_XOUT_L      0x44
-#define MPU6050_RA_GYRO_YOUT_H      0x45
-#define MPU6050_RA_GYRO_YOUT_L      0x46
-#define MPU6050_RA_GYRO_ZOUT_H      0x47
-#define MPU6050_RA_GYRO_ZOUT_L      0x48
+//#define MPU6050_RA_GYRO_XOUT_L      0x44
+//#define MPU6050_RA_GYRO_YOUT_H      0x45
+//#define MPU6050_RA_GYRO_YOUT_L      0x46
+//#define MPU6050_RA_GYRO_ZOUT_H      0x47
+//#define MPU6050_RA_GYRO_ZOUT_L      0x48
 #define MPU6050_RA_PWR_MGMT_1       0x6B
 
 Servo rollServo;
